74b6f174beb13f8c29ceb6ac035a8b9321063867
Maximilian Gerlach <m@px0.de>
2014-03-31 07:10:55 -0400

    return a json array instead of a fragment because thats easier

--- sinatra-api-stub/app.rb
+++ sinatra-api-stub/app.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
+require 'json'
 
 get '/api/beacon/:uuid/:major/:minor' do
-	"'http://dummyimage.com/800x600/292929/e3e3e3&text=#{params[:uuid]}-#{params[:major]}-#{params[:minor]}'"
+	content_type :json
+	{:url => "http://dummyimage.com/800x600/292929/e3e3e3&text=#{params[:uuid]}-#{params[:major]}-#{params[:minor]}"}.to_json
 end
 
 
